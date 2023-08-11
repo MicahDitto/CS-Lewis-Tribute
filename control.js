0e54760111830c9ef40dd84325e481b49832c833
Micah Dittmar <Micah.dittmar@gmail.com>
2023-08-11 19:11:55 -0400

    Fixed timeline and most styles. Added Carousels and functionality.

--- control.js
+++ control.js
@@ -8,3 +8,26 @@ books.forEach(book => {
     });
 });
 
+const imageScroller = document.querySelector('.image-scroller');
+
+let isDragging = false;
+let startX;
+
+imageScroller.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    startX = e.pageX - imageScroller.offsetLeft;
+});
+
+window.addEventListener('mousemove', (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - imageScroller.offsetLeft;
+    const walk = (x - startX) * 3; // Adjust the multiplier for scrolling speed
+    imageScroller.scrollLeft = imageScroller.scrollLeft - walk;
+});
+
+window.addEventListener('mouseup', () => {
+    isDragging = false;
+});
+
+
